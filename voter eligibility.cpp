75d9cbd94c127b04d9508f4b62e8db61df44eba9
brian-kip <113587587+brian-kip@users.noreply.github.com>
2022-10-12 17:50:57 +0300

    Add files via upload

---
+++ voter eligibility.cpp
@@ -0,0 +1,17 @@
+//voter eligibility//
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	int age;
+	cout <<"enter age"<<endl;
+	cin>>age>>endl;
+	if (age>=18) {
+		cout<<"you are eligible to vote"<<endl;
+	}
+	else{
+		cout<<"not eligible"<<;endl
+}
+return 0;
+}
