d99033999a5ef6d7aa9359a82ac603dfb71529e4
brian-kip <113587587+brian-kip@users.noreply.github.com>
2022-10-12 17:52:45 +0300

    Add files via upload

---
+++ temperature.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math>include 
+using namespace std;
+int main() {
+	float cel,far;
+	cout<<"enter temperature in farenheit:";
+	cin>>far;;
+	cel=(far-32)*5/9;{
+		cout<<"temperature in celsius is:"
+		<<cel<<endl;
+		
+	}
+	return 0;
+}
