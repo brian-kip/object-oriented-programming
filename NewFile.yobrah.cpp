b14980dad384023845ffd2f13e17552684e2e6f4
brian-kip <113587587+brian-kip@users.noreply.github.com>
2022-11-24 12:18:38 +0300

    Add files via upload

---
+++ NewFile.yobrah.cpp
@@ -0,0 +1,27 @@
+//2 dimensional array
+#include<iostream>
+#define size 16
+using namespace std;
+int main()
+int (size)(size)
+cout<<"Enter the number of rows"<<endl;
+cin>>m;
+cout<<"Enter the number of columns"<<endi;
+cin>>n;
+
+cout<<"Enter the elements in table"<<endl;
+for(int i=0;i<m;i++)
+{
+for(int j=0;j<n;j++)
+{
+	cin>>a(i)(j);
+}
+}
+for(int i=0;i<m;i++)
+for(int j=0;j<n;j++)
+{
+	cout<<"a("<<i<<")("<<j<<"):";
+	cout<<a(i)(j)<<endl;
+}
+return 0;
+}
